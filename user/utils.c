b3c3ed7bb8b21853c8df293034557627b00de9d5
Stephan Jaekel <steph@rdev.info>
2016-01-08 08:33:09 +0100

    Added voltage reading.

--- user/utils.c
+++ user/utils.c
@@ -30,8 +30,7 @@ void ICACHE_FLASH_ATTR post_sensors(void) {
 	
 	if (current_post == POST_HUMIDITY) {
 		current_post = POST_VOLTAGE;
-		// post_value("voltage", 0);
-		wifi_station_disconnect();
+		post_value("voltage", (uint8_t)system_system_get_vdd33());
 		return;
 	}
 	
